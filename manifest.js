aa2390fe062fbcdcbb9c86969eab966bb0267032
charithapammi-afk <charithapammi@gmail.com>
2025-10-19 15:23:11 +0530

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,20 +1,12 @@
 {
-  "name": "My Demo App",
-  "short_name": "DemoApp",
-  "start_url": "/",
-  "display": "standalone",
-  "background_color": "#ffffff",
-  "theme_color": "#007bff",
-  "icons": [
-    {
-      "src": "icon-12.png",
-      "sizes": "192x192",
-      "type": "image/png"
-    },
-    {
-      "src": "icon-123.png",
-      "sizes": "512x512",
-      "type": "image/png"
-    }
-  ]
+"name": "My Demo App",
+"short_name": "DemoApp",
+"start_url": "/",
+"display": "standalone",
+"background_color": "#ffffff",
+"theme_color": "#007bff",
+"icons": [
+{"src": "icon-192.png", "sizes": "192x192", "type": "image/png"},
+{"src": "icon-512.png", "sizes": "512x512", "type": "image/png"}
+]
 }
